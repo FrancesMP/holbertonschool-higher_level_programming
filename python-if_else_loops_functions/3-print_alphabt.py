fda3e5f4970ffc3fd40ebc59a7d2d9a732f4267f
Frances Palmer <francespalmer@Francess-MacBook-Air.local>
2025-09-07 22:37:43 +0200

    fix of syntax

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter != 113 and letter != 101:
-        print("{}".format(chr(letter)), end="")
-    
+
+print("".join(chr(i) for i in range(97, 123) if i not in (101, 113)), end="")
