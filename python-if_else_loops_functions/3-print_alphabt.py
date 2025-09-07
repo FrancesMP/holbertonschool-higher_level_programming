7d88b58f72fbdb1327007c9d6ea1d684a833048f
Frances Palmer <francespalmer@Francess-MacBook-Air.local>
2025-09-07 22:24:14 +0200

    fixing task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-Print the lowercase alphabet, excluding the letters 'e' and 'q'.
-This script uses a for loop to iterate through ASCII codes of lowercase letters (97-122)
-and skips characters corresponding to 'e' (code 101) and 'q' (code 113).
-"""
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end=""
-              )
+for letter in range(97, 123):
+    if letter != 113 and letter != 101:
+        print("{}".format(chr(letter)), end="")
+    
