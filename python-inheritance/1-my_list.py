9da70578a63c0d3ca9384c3697ee19b79f24b4e3
Frances Palmer <francespalmer@Francess-MacBook-Air.local>
2025-09-26 22:17:41 +0200

    tasks 1 again

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        value = int(value)
-        print ("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+"""
+This module defines a class MyList that inherits from the built-in list.
+It adds a public method to print the list in ascending sorted order.
+"""
+
+
+class MyList(list):
+    """
+    MyList class inherits from list and adds
+    a method to print a sorted version of the list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order without
+        modifying the original list.
+        """
+        print(sorted(self))
