542c4b27d72e255a19cf9247e94580c2c7ed8bee
Frances Palmer <francespalmer@Francess-MacBook-Air.local>
2025-09-26 22:10:38 +0200

    correction task 1

--- python-exceptions/1-main.py
+++ python-exceptions/1-main.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+MyList = __import__('1-my_list').MyList
 
-# Test 1: entier positif
-value = 42
-print("Return:", safe_print_integer(value))  # Affiche: 42 + Return: True
-
-# Test 2: negative integer
-value = -3
-print("Return:", safe_print_integer(value))  # Affiche: -3 + Return: True
-
-# Test 3: zero
-value = 0
-print("Return:", safe_print_integer(value))  # Affiche: 0 + Return: True
-
-# Test 4: string
-value = "abc"
-print("Return:", safe_print_integer(value))  # Rien + Return: False
-
-# Test 5: float
-value = 3.14
-print("Return:", safe_print_integer(value))  # Rien + Return: False
-
-# Test 6: boolean
-value = True
-print("Return:", safe_print_integer(value))  # Affiche: 1 + Return: True
-
-# Test 7: liste
-value = [1, 2, 3]
-print("Return:", safe_print_integer(value))  # Rien + Return: False
-
-# Test 8: dictionnaire
-value = {"a": 1}
-print("Return:", safe_print_integer(value))  # Rien + Return: False
-
-# Test 9: None
-value = None
-print("Return:", safe_print_integer(value))  # Rien + Return: False
-
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
