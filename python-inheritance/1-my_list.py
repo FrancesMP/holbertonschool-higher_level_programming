8ea482cf4620b1b104b282ccef79b690b9891a13
Frances Palmer <francespalmer@Francess-MacBook-Air.local>
2025-09-26 22:12:13 +0200

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-
-"""
-    Module with class MyList
-"""
-
-
-class MyList(list):
-    """
-    MyList class that inherit from list
-
-    Args:
-        list (list): list of int
-    """
-    def print_sorted(self):
-        """
-        print_sorted sort the list and display the result
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+def safe_print_integer(value):
+    try:
+        value = int(value)
+        print ("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
